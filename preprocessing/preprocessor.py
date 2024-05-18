9fc360fc7f8492b1d5a2a3576ecd388d2a8b8579
Gaaaamma <ytching552022@gmail.com>
2024-05-18 17:33:17 +0800

    Feat: preprocessor - cut raw data height successfully

---
+++ preprocessing/preprocessor.py
@@ -0,0 +1,89 @@
+"""
+Preprocessor is responsible for get the position of arrows
+and cut it into 4 small pictures for training
+"""
+
+from typing import List
+
+import cv2 as cv
+
+from setting import SETTINGS
+
+def blue_calculator(file: str) -> List[int]:
+    """Read from a image file and calculate blue point of each row"""
+    result: List[int] = []
+    img = cv.imread(file)
+    print(img.shape)
+    
+    for row in range(img.shape[0]):
+        counter: int = 0
+        for col in range(img.shape[1]):
+            if is_blue(img, row, col):
+                counter += 1
+        result.append(counter)
+    
+    return result
+
+def is_blue(img, row: int, col: int) -> bool:
+    """Check if a certain point in img is blue"""
+
+    if img[row][col][0] == 255 and img[row][col][1] == 0 and img[row][col][2] == 0:
+        return True
+    return False
+
+def cut(file: str) -> bool:
+    """Use blue calculator to determine the cut height"""
+
+    img_top: int = 0
+    img_btm: int = 0
+    threshold: int = SETTINGS.laplace_blue_threshold
+    toleration: int = SETTINGS.laplace_blue_tolerantion
+    statistic: List[int] = blue_calculator(file)
+    statistic = statistic[SETTINGS.laplace_blue_height_start:SETTINGS.laplace_blue_height_end]
+    
+    for row, count in enumerate(statistic):
+        if count >= threshold:
+            up: int = row - SETTINGS.captcha_height
+            if up > 0 and statistic[up] >= threshold - toleration:
+                img_top, img_btm = up , row
+                img_top += SETTINGS.laplace_blue_height_start
+                img_btm += SETTINGS.laplace_blue_height_start
+                print(f"[Cut][Up] find img_top: {img_top}, img_btm: {img_btm}")
+                print(f"[Cut][Up] blue points: ({statistic[up]}, {statistic[row]})")
+                break
+            
+            down: int = row + SETTINGS.captcha_height
+            if down < len(statistic) and statistic[down] >= threshold - toleration:
+                img_top, img_btm = row , down
+                img_top += SETTINGS.laplace_blue_height_start
+                img_btm += SETTINGS.laplace_blue_height_start
+                print(f"[Cut][Down] find img_top: {img_top}, img_btm: {img_btm}")
+                print(f"[Cut][Down] blue points: ({statistic[row]}, {statistic[down]})")
+                break
+    
+    if img_top == 0 and img_btm == 0:
+        print(f"[Cut] Not found")
+        return False
+    
+    # Cut original file
+    file = file.split("_")[-1]
+    file = f"{SETTINGS.raw_data_dir}{file}"
+    img = cv.imread(file)
+    img = img[img_top:img_btm + 1]
+
+    cv.imshow("image_cut_hight", img)
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    return True
+    
+    
+
+
+if __name__ == "__main__": 
+    file: str = f"{SETTINGS.laplace_data_dir}lap_1716017463.png"
+    # file: str = f"{SETTINGS.laplace_data_dir}lap_1716018461.png"
+    # file: str = f"{SETTINGS.laplace_data_dir}lap_1716018475.png"
+    # file: str = f"{SETTINGS.laplace_data_dir}lap_1716018501.png"
+
+    result: bool = cut(file)
--- setting.py
+++ setting.py
@@ -5,7 +5,13 @@ class Settings(BaseSettings):
     screenshot_height: int = 600
     raw_data_dir: str = "./data/raw/"
     laplace_data_dir: str = "./data/laplace/"
-    laplace_blue_threshold: int = 300
+    
+    laplace_blue_threshold: int = 240
+    laplace_blue_tolerantion: int = 50
+    laplace_blue_height_start: int = 150
+    laplace_blue_height_end: int = screenshot_height / 2
+
+    captcha_height: int = 80
 
     class Config:
         env_file: str = ".env"
