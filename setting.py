563596ae4e123a58533db466b602715ea8a63d25
Gaaaamma <ytching552022@gmail.com>
2024-05-18 12:23:09 +0800

    Update: setting - get setting from .env

--- setting.py
+++ setting.py
@@ -1,6 +1,9 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    name: str = "for test"
+    name: str
+
+    class Config:
+        env_file: str = ".env"
     
 SETTINGS: Settings = Settings()
