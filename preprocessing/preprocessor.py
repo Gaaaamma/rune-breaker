e574ea482afff74a3c82726cfc419685d3a0bb2c
Gaaaamma <ytching552022@gmail.com>
2024-05-19 10:58:44 +0800

    Chore: simple test

--- preprocessing/preprocessor.py
+++ preprocessing/preprocessor.py
@@ -81,17 +81,17 @@ def cut(file: str) -> bool:
     file = f"{SETTINGS.raw_data_dir}{file}"
     img = cv.imread(file)
     img = img[img_top:img_btm + 1, 180:620]
-    print(f"img.shape: {img.shape}")
     
     cv.imshow(file, img)
     
     _, img = cv.threshold(img, 200, 255, cv.THRESH_BINARY)
-    cv.imshow("thrshold", img)
+    cv.imshow("thrshold_binary", img)
     
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    # _, img = cv.threshold(img, 50, 255, cv.THRESH_BINARY_INV)
-    img = cv.Canny(img, 100, 200)
-    cv.imshow("after", img)
+    # _, img = cv.threshold(img, 50, 255, cv.THRESH_BINARY)
+    # img = cv.Canny(img, 100, 200)
+    cv.imshow("gray", img)
+    print(f"img.shape: {img.shape}")
 
     cv.waitKey(0)
     cv.destroyAllWindows()
