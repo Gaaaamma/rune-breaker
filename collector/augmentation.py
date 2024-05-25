8ed5564d8bae28addfaa486f7114aba1293c84fa
Gaaaamma <ytching552022@gmail.com>
2024-05-25 21:31:48 +0800

    Feat: flip standard image to get more training data

--- collector/augmentation.py
+++ collector/augmentation.py
@@ -3,7 +3,7 @@ import os
 from setting import SETTINGS
 from typing import List
 
-def rotate_right_90(image_path: str, save_path: str):
+def augmentation(image_path: str, save_path: str):
     mapping: List[str] = ['w', 'd', 's', 'a']
 
     # Get direction and find index
@@ -20,31 +20,25 @@ def rotate_right_90(image_path: str, save_path: str):
         direction = mapping[directoin_index]
         image = image.rotate(-90, expand=True)
         path: str = f"{save_path}{prefix}{direction}.png"
+        flip_path: str = f"{save_path}f{prefix}{direction}.png"
         print(path)
         image.save(path)
 
-def flip_horizontal(image_path, save_path):
-    # 讀取圖片
-    image = Image.open(image_path)
-    # 水平翻轉
-    flipped_image = image.transpose(Image.FLIP_LEFT_RIGHT)
-    # 保存圖片
-    flipped_image.save(save_path)
-
-def flip_vertical(image_path, save_path):
-    # 讀取圖片
-    image = Image.open(image_path)
-    # 垂直翻轉
-    flipped_image = image.transpose(Image.FLIP_TOP_BOTTOM)
-    # 保存圖片
-    flipped_image.save(save_path)
+        if direction == "w" or direction == "s":
+            # 水平翻轉
+            flipped_image = image.transpose(Image.FLIP_LEFT_RIGHT)
+            flipped_image.save(flip_path)
+        elif direction == "a" or direction == "d":
+            # 垂直翻轉
+            flipped_image = image.transpose(Image.FLIP_TOP_BOTTOM)
+            flipped_image.save(flip_path)
 
 if __name__ == "__main__":
     # Get all standard images
     images = os.listdir(f"{SETTINGS.standard_data_dir}")
+    images = map(lambda img: f"{SETTINGS.standard_data_dir}{img}", images)
     images = sorted(images)
-    for i, v in enumerate(images):
-        images[i] = f"{SETTINGS.standard_data_dir}{v}"
-    
+
     for img in images:
-        rotate_right_90(img, SETTINGS.train_data_dir)
+        print(img)
+        augmentation(img, SETTINGS.train_data_dir)
