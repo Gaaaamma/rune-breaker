47bf8229bbe546a48a3a15e4094a100854b00b53
Gaaaamma <ytching552022@gmail.com>
2024-10-13 16:05:45 +0800

    Update: boss - throw item delay time to 0.5

--- controller/boss.py
+++ controller/boss.py
@@ -133,7 +133,7 @@ class BossControl():
         if throw_setting:
             # Open inventory
             self.comm.key(InventoryControl.inventory.shortcut)
-            time.sleep(1)
+            time.sleep(0.5)
 
             # Get category coordination
             category_x, category_y = InventoryControl.get_category_coordination(
@@ -142,32 +142,32 @@ class BossControl():
 
             # Move to specified category and click
             self.comm.move_cursor_to(category_x, category_y)
-            time.sleep(1)
+            time.sleep(0.5)
             self.comm.click_cursor("LEFT")
-            time.sleep(1)
+            time.sleep(0.5)
 
             # Move to specified item and click
             item_x, item_y = InventoryControl.get_item_coordination(
                 throw_setting.item_index_x, throw_setting.item_index_y
             )
             self.comm.move_cursor_to(item_x, item_y)
-            time.sleep(1)
+            time.sleep(0.5)
             self.comm.click_cursor("LEFT")
-            time.sleep(1)
+            time.sleep(0.5)
 
             # Move to spare position and click
             self.comm.move_cursor_to(
                 InventoryControl.inventory.item.spare_x, InventoryControl.inventory.item.spare_y
             )
-            time.sleep(1)
+            time.sleep(0.5)
             self.comm.click_cursor("LEFT")
-            time.sleep(1)
+            time.sleep(0.5)
 
             # Check multiple items to check if we need to type number to throw
             if throw_setting.multiple_item:
                 # Just throw one item out of inventory
                 self.comm.key("1", open_chat=False, close_chat=True)
-                time.sleep(1)
+                time.sleep(0.5)
             
             # Close inventory
             self.comm.key(InventoryControl.inventory.shortcut)
